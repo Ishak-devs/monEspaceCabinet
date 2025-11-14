2179cbe3fdb5aa0e867352a5be202ed50ebc2d79
ishak-devs <kouicicontact@yahoo.com>
2025-11-14 13:50:40 +0100

    fix

--- version/update/update_app.py
+++ version/update/update_app.py
@@ -15,17 +15,17 @@ def update_app(download_url, GITHUB_TOKEN):
         shutil.move(new_exe, temp_exe)
 
         updater_script = f"""
-import shutil, time, sys, os
+import shutil, time, sys, os, subprocess
 time.sleep(1)  
 shutil.move(r'{temp_exe}', r'{current_exe}')
 subprocess.Popen([r'{current_exe}'])
 """
      
-    print('update...')
-    temp_updater = os.path.join(os.path.dirname(current_exe), "updater_temp.py")
-    with open(temp_updater, "w") as f:
-        f.write(updater_script)
+        print('update...')
+        temp_updater = os.path.join(os.path.dirname(current_exe), "updater_temp.py")
+        with open(temp_updater, "w") as f:
+            f.write(updater_script)
 
-    subprocess.Popen([sys.executable, temp_updater])
+        subprocess.Popen([sys.executable, temp_updater])
 
-    sys.exit(0)
+        sys.exit(0)
