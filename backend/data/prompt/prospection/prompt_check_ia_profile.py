24d6cb40cd2dc679aa4c5b66f9fd8f100e159fa4
ishak <i.kouici@insta.fr>
2026-02-06 11:52:39 +0100

    update prompt to add var is match and is top and send it to methode

--- backend/data/prompt/prospection/prompt_check_ia_profile.py
+++ backend/data/prompt/prospection/prompt_check_ia_profile.py
@@ -8,14 +8,17 @@ def prompt_check_ia_profile(offre, profile_main_content):
         Analyse ce profil LinkedIn : {profile_main_content}
         Est-ce qu'il correspond à cette {offre} ?
         Réponds uniquement par 'OUI' ou 'NON'.
-        Si le profil semble être d'une correspondance réellement idéal, réponds 'OUI TOP'
+        Si le profil semble être d'une correspondance réellement idéal, réponds 'OUI TOP' et l'url de ce profil.
         """
 
     response_ia = call_groq(prompt) or ""
     response_clear = response_ia.strip().lower()
+    parts = response_clear.split()
+    is_match = "oui" in parts
+    is_top = "top" in parts
     print(f"Reponse IA: {response_clear}")
     print(
         f"✅ [IA CHECK] Verdict: {'Accepté' if 'OUI' in response_clear else 'Refusé'}"
     )
 
-    return "OUI" in response_clear, "top" in response_clear
+    return is_match, is_top
