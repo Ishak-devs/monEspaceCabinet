be494ca7f914d03e086467dc70cb5aee3db9a311
ishak <i.kouici@insta.fr>
2026-02-10 09:35:19 +0100

    init query to get name and telephone

--- backend/workflow/start_prospect_auto.py
+++ backend/workflow/start_prospect_auto.py
@@ -77,8 +77,13 @@ def start_prospect_auto():
                             job["linkedin_password"] = decrypted_data.get(
                                 "linkedin_password"
                             )
+                            job["full_name"] = decrypted_data.get("full_name")
+                            job["telephone"] = decrypted_data.get("telephone")
+
                             print(f"linkedin_email: {job['linkedin_email']}")
                             print(f"linkedin_password: {job['linkedin_password']}")
+                            print(f"full_name: {job['full_name']}")
+                            print(f"telephone: {job['telephone']}")
                         else:
                             print("DEBUG - Invalid settings format")
                     else:
