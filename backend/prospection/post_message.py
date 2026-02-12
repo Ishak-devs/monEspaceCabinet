db4a1d1fd5c3877aecef006a54cf7ead3b900d7c
ishak <i.kouici@insta.fr>
2026-02-12 13:58:01 +0100

    update and add actions chain to write

--- backend/prospection/post_message.py
+++ backend/prospection/post_message.py
@@ -2,6 +2,7 @@ import random
 import time
 
 from data.prompt.post_prompt import post_prompt
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -12,7 +13,12 @@ from data.call_groq import call_groq
 def post_message(driver, post):
 
     try:
-        wait = WebDriverWait(driver, 10)
+        instruction = "Aide nous à généré un message pour Linkedin"
+        prompt = post_prompt(instruction)
+        message_ia = call_groq(prompt)
+        print(f"Message généré : {message_ia}")
+
+        wait = WebDriverWait(driver, 30)
         post_input = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable(
                 (By.XPATH, "//p[contains(text(), 'Commencer un post')]")
@@ -24,21 +30,27 @@ def post_message(driver, post):
 
         editor = wait.until(
             EC.presence_of_element_located(
-                (By.CSS_SELECTOR, "div[role='textbox'], ql-editor")
+                (By.CSS_SELECTOR, "div[role='textbox'], .ql-editor")
             )
         )
-
-        instruction = "Aide nous à généré un message pour Linkedin"
-        prompt = post_prompt(instruction)
-        message_ia = call_groq(prompt)
-        print(f"Message généré : {message_ia}")
+        print("Editor found")
 
         if message_ia:
+            driver.execute_script("arguments[0].focus();", editor)
+            time.sleep(5)
             editor.click()
+            actions = ActionChains(driver)
+            actions.move_to_element(editor)
+            actions.click()
+            actions.perform()
+
+            print("Editor clicked")
+
             for char in message_ia:
-                editor.send_keys(char)
-                time.sleep(0.05)
+                actions.send_keys(char)
+                actions.perform()
+                time.sleep(random.uniform(0.02, 0.06))
                 print(f"Char sent: {char}")
 
     except Exception as e:
-        print(f"Erreur lors de l'ouverture du message : {e}")
+        print(f"Erreur : {e}")
