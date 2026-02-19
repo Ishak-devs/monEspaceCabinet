c6ac94aa0a97c86145b13d695e0dd5bf5c92aa6f
ishak <i.kouici@insta.fr>
2026-02-19 15:53:41 +0100

    add linkedin logo

--- front/src/composants/ProspectionSourcing.jsx
+++ front/src/composants/ProspectionSourcing.jsx
@@ -178,6 +178,7 @@ function ProspectionSourcing() {
       if (response.ok) {
         setIntitule("");
         setDetails("");
+        setSegment("");
         setOffre("");
         setPost("");
         setMode("");
@@ -198,9 +199,20 @@ function ProspectionSourcing() {
     <div className="min-h-screen bg-white p-4 md:p-6 font-sans">
       <div className="max-w-5xl mx-auto">
         <div className="mb-6">
-          <h1 className="text-lg font-normal text-gray-900 tracking-tight">
-            Prospection et sourcing
-          </h1>
+          <div className="flex items-center gap-2">
+            <h1 className="text-lg font-normal text-gray-900 tracking-tight">
+              Prospection et sourcing
+            </h1>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="#0077B5"
+            >
+              <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.238 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+            </svg>
+          </div>
           <p className="text-gray-500 text-xs mt-0.5">Gestion</p>
         </div>
 
@@ -214,16 +226,16 @@ function ProspectionSourcing() {
                   <input
                     type="radio"
                     name="filtre"
-                    value="entreprise"
+                    value="Entreprises"
                     onChange={(e) => setSegment(e.target.value)}
                   />{" "}
-                  Entreprise
+                  Entreprises
                 </label>
                 <label>
                   <input
                     type="radio"
                     name="filtre"
-                    value="offres"
+                    value="Offres"
                     onChange={(e) => setSegment(e.target.value)}
                   />{" "}
                   Offres
@@ -232,7 +244,7 @@ function ProspectionSourcing() {
                   <input
                     type="radio"
                     name="filtre"
-                    value="annonces"
+                    value="Annonces"
                     onChange={(e) => setSegment(e.target.value)}
                   />{" "}
                   Annonces
@@ -241,7 +253,7 @@ function ProspectionSourcing() {
                   <input
                     type="radio"
                     name="filtre"
-                    value="personnes"
+                    value="Personnes"
                     onChange={(e) => setSegment(e.target.value)}
                   />{" "}
                   Personnes
@@ -267,6 +279,7 @@ function ProspectionSourcing() {
                       />
                     </div>
                     {/* Input Métier */}
+                    {/* {mode === "sourcing" && (*/}
                     <div>
                       <label
                         htmlFor="intitule"
@@ -285,6 +298,8 @@ function ProspectionSourcing() {
                         // required
                       />
                     </div>
+                    {/* </div>
+                    )}*/}
 
                     {/* Input Détails */}
                     <div>
@@ -305,37 +320,6 @@ function ProspectionSourcing() {
                         // required
                       />
                     </div>
-                    {/* <div>
-                      <label className="block text-xs font-normal text-gray-600 mb-2">
-                        Type d'analyse
-                      </label>
-                      <div className="flex gap-4">
-                        <label className="flex items-center text-xs font-normal text-gray-700 cursor-pointer">
-                          <input
-                            type="radio"
-                            name="people_selection"
-                            value="people_precise"
-                            checked={people === "people_precise"}
-                            onChange={(e) => Setpeople_precise(e.target.value)}
-                            disabled={isLoading}
-                            className="mr-2 accent-black"
-                          />
-                          Recherche inteligente
-                        </label>
-                        <label className="flex items-center text-xs font-normal text-gray-700 cursor-pointer">
-                          <input
-                            type="radio"
-                            name="people_selection"
-                            value="people_not_precise"
-                            checked={people === "people_not_precise"}
-                            onChange={(e) => Setpeople_precise(e.target.value)}
-                            disabled={isLoading}
-                            className="mr-2 accent-black"
-                          />
-                          Contacter du monde
-                        </label>
-                      </div>
-                    </div>*/}
 
                     {/* Radio Buttons Type */}
                     <div>
