4b661d2e85d74e870dd1874ee9cbd5fa0a4e63b6
ishak <i.kouici@insta.fr>
2026-02-19 09:44:38 +0100

    change xpath

--- backend/prospection/send_message.py
+++ backend/prospection/send_message.py
@@ -298,21 +298,24 @@ def send_message(
                 print("On va tenter de fermer la fenêtre")
                 yield "On va fermer la fenêtre de discussion..."
 
-                close_button = driver.find_elements(
-                    By.XPATH, "//button[.//svg[@data-test-icon='close-small']]"
+                close_button = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, "//button[contains(., 'Fermer')]")
+                    )
                 )
-                WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable(close_button[0])
-                )
-                driver.execute_script("arguments[0].click();", close_button[0])
+
+                driver.execute_script("arguments[0].click();", close_button)
+
                 print("Une fenêtre de discussion a été fermée.")
                 yield "Fenêtre fermée... On passe au profil suivant"
 
             except Exception as e:
                 print(f"Erreur lors de la fermeture des fenêtres : {e}")
+                print(f"Erreur bouton close :\n{traceback.format_exc()}")
 
         except Exception as e:
-            print(e)
+            print(f"Erreur : {e}")
+            print(f"Erreur :\n{traceback.format_exc()}")
             continue
 
         finally:
