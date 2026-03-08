907804b8799969d7f724e1fbf031f894e56c0815
Ishak <i.kouici@insta.fr>
2026-03-08 17:03:33 +0100

    ecriture de duree exp

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/section_esperiences.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/section_esperiences.py
@@ -30,3 +30,12 @@ def section_competences(doc, data):
         run.font.bold = True
         run.font.size = Pt(11)
         run.font.color.rgb = COULEUR_PRINCIPALE
+
+        cell_milieu = table.cell(0, 1)
+        p_milieu = cell_milieu.paragraphs[0]
+        p_milieu.alignment = WD_ALIGN_PARAGRAPH.CENTER
+        run2 = p_milieu.add_run(exp.get('Durée_expérience', ''))
+        run2.font.name = POLICE
+        run2.font.size = Pt(10)
+        run2.font.color.rgb = RGBColor(0x77, 0x77, 0x77)
+
