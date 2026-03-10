4a8bc49f3c1d90054619fc929c863ed41972af76
ishak <i.kouici@insta.fr>
2026-03-10 15:10:06 +0100

    const url to vercel

--- system/client/src/composants/dossier_competences.js
+++ system/client/src/composants/dossier_competences.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
-const API_URL = "http://127.0.0.1:8000";
-// const API_URL = "https://filltemplate.onrender.com";
+//const API_URL = "http://127.0.0.1:8000";
+ const API_URL = "https://filltemplate.onrender.com";
 function CVUploadForm() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [addSkills, setAddSkills] = useState(null);
