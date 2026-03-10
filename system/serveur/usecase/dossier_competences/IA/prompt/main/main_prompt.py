8f90d366bc5bbeb10af0ab25f53e0c831ab5a104
ishak <i.kouici@insta.fr>
2026-03-10 15:03:17 +0100

    revu du prompt

--- system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
+++ system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
@@ -1,8 +1,6 @@
 def main_prompt(cv_text, current_template):
 
-    return f"""Tu es un expert en extraction de données. ton but est de structurer dans le json : {current_template} 
-    tout le contenu dans son intégralité sans commentaires,
-    si une clé contient 'à définir', tu dois générer toi-même une valeur, pour les valeurs non trouvé laisse vide.
-DATA:
-{cv_text}
+    return f"""Tu es un expert en extraction de données. Consigne strict : ton but est d'extraire tout le contenu de {cv_text} dans le json : {current_template} 
+    dans son intégralité sans commentaires.
+    Si une clé contient 'à définir', tu dois générer toi-même une valeur, pour les valeurs non trouvé laisse vide.
 """
