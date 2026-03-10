4dc44892f1470271cf6fcd7a6b822899a7854c25
ishak <i.kouici@insta.fr>
2026-03-10 08:47:01 +0100

    ajout de enderline sur le titre du poste

--- system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/experiences/display_poste.py
+++ system/serveur/usecase/dossier_competences/services/library/python_docx/build_dossier/body/sections/experiences/display_poste.py
@@ -5,7 +5,9 @@ def display_poste(doc, exp):
     p_poste = doc.add_paragraph()
     run_poste = p_poste.add_run(str(exp.get('Poste_Occupé', '')).upper())
     run_poste.font.color.rgb = RGBColor(0x00, 0x20, 0x60)
+
     run_poste.bold = True
+    run_poste.underline = True
 
     p_poste.paragraph_format.space_before = Pt(10)
     p_poste.paragraph_format.space_after = Pt(10)
