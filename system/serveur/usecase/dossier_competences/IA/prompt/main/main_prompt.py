2f153210e0f167089c37b8481218a28d3b3cdcab
ishak <i.kouici@insta.fr>
2026-03-10 15:00:13 +0100

    changement du prompt main pour dire de laisser les champs vide si il trouve pas j ai aussi supprimé celan prompt car not use

--- system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
+++ system/serveur/usecase/dossier_competences/IA/prompt/main/main_prompt.py
@@ -1,8 +1,8 @@
 def main_prompt(cv_text, current_template):
 
     return f"""Tu es un expert en extraction de données. ton but est de structurer dans le json : {current_template} 
-    tout le contenu dans son intégralité sans commentaires ,
-    si une clé contient 'à définir', tu dois générer toi-même une valeur
+    tout le contenu dans son intégralité sans commentaires,
+    si une clé contient 'à définir', tu dois générer toi-même une valeur, pour les valeurs non trouvé laisse vide.
 DATA:
 {cv_text}
 """
