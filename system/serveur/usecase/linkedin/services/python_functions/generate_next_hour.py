2ac4b9e4ae0bd59432b159187e99467b91421544
ishak <i.kouici@insta.fr>
2026-03-16 15:01:28 +0100

    add next hour check

--- system/serveur/usecase/linkedin/services/python_functions/generate_next_hour.py
+++ system/serveur/usecase/linkedin/services/python_functions/generate_next_hour.py
@@ -1,11 +1,21 @@
 from datetime import datetime, timedelta
 import random
 
+from data.database import supabase_client
 
 def generate_next_hour():
-    generate_hour = (datetime.now().astimezone() + timedelta(days=1)).replace(
-        hour=random.randint(8, 19),
-        minute=random.randint(0, 59),
-    )
+    while True:
+        new_hour = (datetime.now().astimezone() + timedelta(days=1)).replace(
+            hour=random.randint(8, 19),
+            minute=random.randint(0, 59),
+            second=0,
+            microsecond=0
+        )
 
-    return generate_hour
+        check = supabase_client().table("prospection_settings") \
+            .select("id") \
+            .eq("hour_start", new_hour.isoformat()) \
+            .execute()
+
+        if not check.data:
+            return new_hour
