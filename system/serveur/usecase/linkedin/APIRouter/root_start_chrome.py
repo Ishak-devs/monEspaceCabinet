23fe37a180a26c9f9dfcda82b98501d6611bda5b
ishak <i.kouici@insta.fr>
2026-03-16 14:51:00 +0100

    add try catch cabinet get

--- system/serveur/usecase/linkedin/APIRouter/root_start_chrome.py
+++ system/serveur/usecase/linkedin/APIRouter/root_start_chrome.py
@@ -4,7 +4,6 @@ from starlette.responses import StreamingResponse
 from usecase.linkedin.generator.streaming.stream_generator import stream_generator
 from usecase.linkedin.classes.user.UserRequest import  UserRequest
 from usecase.linkedin.objects.user.object_user_data import object_user_data
-from usecase.linkedin.services.python_functions.generate_next_hour import generate_next_hour
 from usecase.linkedin.query.tables.cabinets.get.get_cabinet_id import get_cabinet_id
 from usecase.linkedin.query.tables.prospection_settings.insert.insert_prospection_settings import insert_prospection_settings
 from usecase.linkedin.query.tables.user.get.get_user_id import get_user_id
@@ -18,7 +17,7 @@ async def root_start_chrome(
     body: UserRequest,
     current_user_id: str = Depends(get_user_id),
 ):
-    cabinet_id =  await get_cabinet_id()
+    cabinet_id =  await get_cabinet_id(current_user_id)
     user_data = object_user_data(body, current_user_id)
     user_data = get_user_informations(user_data)
 
--- system/serveur/usecase/linkedin/query/tables/cabinets/get/get_cabinet_id.py
+++ system/serveur/usecase/linkedin/query/tables/cabinets/get/get_cabinet_id.py
@@ -1,20 +1,20 @@
-from fastapi import Depends
-
-from usecase.linkedin.query.tables.user.get.get_user_id import get_user_id
 from data.database import supabase_client
 
 
-async def get_cabinet_id(current_user_id: str = Depends(get_user_id)):
-    cabinet_id = None
-    res_cabinet = (
-        supabase_client().table("profiles")
-        .select("cabinet_id")
-        .eq("id", current_user_id)
-        .limit(1)
-        .execute()
-    )
+async def get_cabinet_id(current_user_id):
+    try:
+        res_cabinet = (
+            supabase_client().table("profiles")
+            .select("cabinet_id")
+            .eq("id", current_user_id)
+            .limit(1)
+            .execute()
+        )
 
-    if res_cabinet.data and isinstance(res_cabinet.data, dict):
-        cabinet_id = res_cabinet.data[0].get("cabinet_id")
+        if res_cabinet.data and len(res_cabinet.data) > 0:
+            return res_cabinet.data[0].get("cabinet_id")
 
-    return cabinet_id
+    except Exception as e:
+        print(f"ERREUR RÉCUPÉRATION CABINET_ID : {e}")
+
+    return None
