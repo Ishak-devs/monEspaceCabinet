bd29987924cad4dcfeaaed13765082241b3a1dde
ishak <i.kouici@insta.fr>
2026-03-16 15:51:05 +0100

    add generate hour whn auto

--- system/serveur/usecase/linkedin/query/tables/prospection_settings/update/update_has_run_today_true.py
+++ system/serveur/usecase/linkedin/query/tables/prospection_settings/update/update_has_run_today_true.py
@@ -1,8 +1,13 @@
 from data.database import supabase_client
+from usecase.linkedin.services.python_functions.generate_next_hour import generate_next_hour
 
 
 def update_has_run_today_true():
-    last_job = supabase_client().table("prospection_settings").select("id").eq("has_run_today", False).order("id", desc=True).limit(1).execute()
+    last_job = (supabase_client().table("prospection_settings").select("id").eq("has_run_today", False)
+                .order("id", desc=True).limit(1).execute())
 
     if last_job.data:
-        return supabase_client().table("prospection_settings").update({"has_run_today": True, "is_active": False}).eq("id", last_job.data[0]["id"]).execute()
+        new_hour = generate_next_hour()
+        return (supabase_client().table("prospection_settings").update({"has_run_today": True, "is_active": False,
+                                                                        "hour_start": new_hour})
+                .eq("id", last_job.data[0]["id"]).execute())
