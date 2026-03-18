5e93111860c71ade2f8c19fc6be0dc326f6176e5
ishak <i.kouici@insta.fr>
2026-03-18 10:59:59 +0100

    insert ok use supabase client

--- system/serveur/query/collaborateurs/ajouter_collaborateur.py
+++ system/serveur/query/collaborateurs/ajouter_collaborateur.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 from data.get_admin_client import get_admin_client
+from data.database import supabase_client
 
 def query_ajouter_collaborateur(body):
 
@@ -12,7 +13,7 @@ def query_ajouter_collaborateur(body):
                 "password": body.password,
             })
 
-            get_admin_client().table("profiles").insert({
+            supabase_client().table("profiles").insert({
                 "id": auth_res.user.id,
                 "email": body.email,
                 # "cabinet_id": body.cabinet_id
