e6d8f906738edca3a9f7581bc4e280e44e5b0d3a
ishak <i.kouici@insta.fr>
2026-03-19 15:04:19 +0100

    Update system/serveur/endpoint/endpoint.py

--- system/serveur/endpoint/endpoint.py
+++ system/serveur/endpoint/endpoint.py
@@ -4,9 +4,9 @@ import uvicorn
 from APIRouter.router_add_collaborator import router_add_collaborator
 from configurations.config_CORS import config_CORS
 from usecase.dossier_competences.APIRouter.root_generate_dossier import router_start_generate_dossier
-from usecase.linkedin.APIRouter.root_start_auto import router_start_auto
-from usecase.linkedin.APIRouter.root_start_chrome import router_start_chrome
-from usecase.linkedin.APIRouter.root_listes import router_listes
+# from usecase.linkedin.APIRouter.root_start_auto import router_start_auto
+# from usecase.linkedin.APIRouter.root_start_chrome import router_start_chrome
+# from usecase.linkedin.APIRouter.root_listes import router_listes
 
 app = FastAPI()
 config_CORS(app)
@@ -17,10 +17,10 @@ async def root():
         "status": "OK",
     }
 
-app.include_router(router_listes)
-app.include_router(router_start_chrome)
+# app.include_router(router_listes)
+# app.include_router(router_start_chrome)
 app.include_router(router_start_generate_dossier)
-app.include_router(router_start_auto)
+# app.include_router(router_start_auto)
 app.include_router(router_add_collaborator)
 
 if __name__ == "__main__":
