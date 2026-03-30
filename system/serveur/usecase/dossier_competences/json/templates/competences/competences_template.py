7fba3c7998a849f81c80fc91855fdb89cd2f098d
ishak <i.kouici@insta.fr>
2026-03-30 10:35:04 +0200

    ajout de template plus explicit$

--- system/serveur/usecase/dossier_competences/json/templates/competences/competences_template.py
+++ system/serveur/usecase/dossier_competences/json/templates/competences/competences_template.py
@@ -1,4 +1,4 @@
 def competences_template():
     return {
-        "15_Compétences_Percutantes_Sous_Forme_De_Phrase_Sans_commencer_Par_Le_Mot_Compétence_Ou_Eperience": [],
+        "15_Phrases_Percutantes_Savoir_Faire_Candidat_commencant_par_un_verbe_a_infinitif": [],
     }
