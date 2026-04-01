0a2bdf242edc819564f44c41f09adbf1fb101d89
ishak <i.kouici@insta.fr>
2026-04-01 12:56:58 +0200

    ajout d infos dans l objet

--- system/client/src/composants/EmailAuto.js
+++ system/client/src/composants/EmailAuto.js
@@ -1,94 +1,15 @@
 import { useState } from "react";
-//
-//function EmailAuto() {
-//  const [candidate, setCandidate] = useState("");
-//  const [mail, setMail] = useState("");
-//  const [notes, setNotes] = useState("");
-//
-//  return (
-//    <div className="space-y-8">
-//      {/* Section Nouveau Process */}
-//      <section className="p-6 border border-blue-100 rounded-lg bg-blue-50/30">
-//        <h3 className="text-sm font-semibold mb-4 flex items-center">
-//          <span className="mr-2">🚀</span> Démarrer un nouveau process
-//        </h3>
-//        <div className="flex gap-3">
-//          <input
-//            type="text"
-//            placeholder="Nom du candidat..."
-//            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
-//            onChange={(e) => setCandidate(e.target.value)}
-//          />
-//             <input
-//            type="text"
-//            placeholder="Point à mentionner dans le mail..."
-//            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
-//            onChange={(e) => setNotes(e.target.value)}
-//          />
-//            <input
-//            type="text"
-//            placeholder="Email du candidat..."
-//            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
-//            onChange={(e) => setMail(e.target.value)}
-//          />
-//          <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded text-xs font-medium transition-colors">
-//            Envoyer
-//          </button>
-//        </div>
-//      </section>
-//
-
-
-//      {/* Section Historique */}
-//      <section>
-//        <h3 className="text-sm font-semibold mb-4 text-gray-700">
-//          Historique des recrutements
-//        </h3>
-//        <div className="bg-white border border-gray-200 rounded-lg overflow-hidden">
-//          <table className="w-full text-left text-sm">
-//            <thead className="bg-gray-50 border-b border-gray-200 text-gray-600">
-//              <tr>
-//                <th className="px-4 py-2 font-medium">Candidat</th>
-//                <th className="px-4 py-2 font-medium">Date</th>
-//                <th className="px-4 py-2 font-medium">Statut</th>
-//              </tr>
-//            </thead>
-//            {/* <tbody className="divide-y divide-gray-100">
-//              {history.map((h) => (
-//                <tr key={h.id} className="hover:bg-gray-50 transition-colors">
-//                  <td className="px-4 py-3 font-medium text-gray-900">
-//                    {h.name}
-//                  </td>
-//                  <td className="px-4 py-3 text-gray-500">{h.date}</td>
-//                  <td className="px-4 py-3">
-//                    <span
-//                      className={`px-2 py-1 rounded-full text-[10px] font-bold uppercase ${
-//                        h.status === "Recruté"
-//                          ? "bg-green-100 text-green-700"
-//                          : h.status === "Refusé"
-//                            ? "bg-red-100 text-red-700"
-//                            : "bg-blue-100 text-blue-700"
-//                      }`}
-//                    >
-//                      {h.status}
-//                    </span>
-//                  </td>
-//                </tr>
-//              ))}
-//            </tbody>*/}
-//          </table>
-//        </div>
-//      </section>
-//    </div>
-//  );
-//}
-//export default EmailAuto;
-
 
 function EmailAuto() {
+
   const [candidate, setCandidate] = useState("");
   const [mail, setMail] = useState("");
-  const [notes, setNotes] = useState("");
+  const [notes_email, setNotes] = useState("");
+  const [remuneration, setRemuneration] = useState("");
+  const [poste, setPoste] = useState("");
+  const [doc, setDoctosend] = useState("");
+  const [prochaine_etape, setProchain_etape] = useState("");
+  const [lieu, setLieu] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleSend = async () => {
@@ -100,7 +21,11 @@ function EmailAuto() {
         body: JSON.stringify({
           nom: candidate,
           email: mail,
-          notes_mail: notes
+          notes_mail: notes_email,
+          poste: poste,
+          doc: doc,
+          prochaine_etape: prochaine_etape,
+          lieu: lieu,
         }),
       });
       if (response.ok) alert("Email envoyé !");
@@ -120,21 +45,45 @@ function EmailAuto() {
         <div className="flex gap-3">
           <input
             type="text"
-            placeholder="Nom du candidat..."
+            placeholder="Nom du candidat"
             className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
             onChange={(e) => setCandidate(e.target.value)}
           />
-          <input
+           <input
             type="text"
-            placeholder="Point à mentionner..."
+            placeholder="Intitulé d'offre"
             className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
-            onChange={(e) => setNotes(e.target.value)}
+            onChange={(e) => setPoste(e.target.value)}
           />
           <input
             type="text"
-            placeholder="Email du candidat..."
+            placeholder="Lieu"
             className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
-            onChange={(e) => setMail(e.target.value)}
+            onChange={(e) => setLieu(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Rémunération"
+            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
+            onChange={(e) => setRemuneration(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Document a transmettre..."
+            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
+            onChange={(e) => setDoctosend(e.target.value)}
+          />
+            <input
+            type="text"
+            placeholder="Prochaines étapes.."
+            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
+            onChange={(e) => setProchain_etape(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Notes personnalisés..."
+            className="flex-1 p-2 text-sm border rounded shadow-sm focus:ring-1 focus:ring-blue-500 outline-none"
+            onChange={(e) => setNotes(e.target.value)}
           />
           <button
             onClick={handleSend}
