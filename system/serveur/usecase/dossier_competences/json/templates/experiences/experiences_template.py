b3aba9ffb4a68af45f95a453f3c4074e645e4785
ishak-devs <144032436+Ishak-devs@users.noreply.github.com>
2026-04-20 12:01:22 +0200

    Rename fields for task list and mission summary

--- system/serveur/usecase/dossier_competences/json/templates/experiences/experiences_template.py
+++ system/serveur/usecase/dossier_competences/json/templates/experiences/experiences_template.py
@@ -5,8 +5,8 @@ def experiences_template():
             "Nom_Entreprise": "",
             "Dates_Période": "",
             "Durée_Mission_Ou_Mission_Toujours_En_Cours": "",
-            "Liste_Tâches": [],
-            "Résumé_mission": "",
+            "Liste_Tâches_En_Phrase_Nominal": [],
+            "Résumé_mission_En_Phrase_Nominal": "",
             "Logiciels_et_outils_utilisés_Sans_Indiquer_Le_Niveau": [],
         }]
     }
